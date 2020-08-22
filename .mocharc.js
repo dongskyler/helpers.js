2f779029b2f70b1c93dd93e18bf3ed309e5b35ec
Skyler Dong <dongskyler@gmail.com>
2020-08-22 02:06:46 -0500

    Migrate from Jest to Mocha/Chai (#10)
    * Migrate from Jest to Mocha/Chai
    
    * Config istanbul with Codecov and coverage reporter
    
    * Config yarn test script

---
+++ .mocharc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  diff: true,
+  extension: ['ts', 'js'],
+  package: './package.json',
+  reporter: 'spec',
+  slow: 75,
+  timeout: 2000,
+  ui: 'bdd',
+};
--- src/test/sort.spec.ts
+++ src/test/sort.spec.ts
@@ -2,6 +2,7 @@
  * Test sort
  */
 
+import { expect } from 'chai';
 import {
   comparatorLexic,
   comparatorNumeric,
@@ -11,7 +12,7 @@ import {
 } from '../sort';
 
 describe('Test sort', () => {
-  test('Test bubbleSort on ascending lexicographical order.', () => {
+  it('Test bubbleSort on ascending lexicographical order.', () => {
     const input = [
       ['Volvo', 'Ford', 'BMW', 'Mazda'],
       ['one', 'two', 'three', 'four', 'five', 'five'],
@@ -26,10 +27,10 @@ describe('Test sort', () => {
       ['five', 'five', 'four', 'one', 'three', 'two'],
     ];
 
-    expect(output).toStrictEqual(answerKey);
+    expect(output).to.deep.equal(answerKey);
   });
 
-  test('Test bubbleSort on decending lexicographical order.', () => {
+  it('Test bubbleSort on decending lexicographical order.', () => {
     const input = [
       ['Volvo', 'Ford', 'BMW', 'Mazda'],
       ['one', 'two', 'three', 'four', 'five', 'five'],
@@ -44,20 +45,20 @@ describe('Test sort', () => {
       ['two', 'three', 'one', 'four', 'five', 'five'],
     ];
 
-    expect(output).toStrictEqual(answerKey);
+    expect(output).to.deep.equal(answerKey);
   });
 
-  test('Test bubbleSort on ascending numerical order.', () => {
+  it('Test bubbleSort on ascending numerical order.', () => {
     const input = [[6, 5, 6, 1, 2, 3, 0, 4]];
 
     const output = input.map((e) => bubbleSort(e, comparatorNumeric()));
 
     const answerKey = [[0, 1, 2, 3, 4, 5, 6, 6]];
 
-    expect(output).toStrictEqual(answerKey);
+    expect(output).to.deep.equal(answerKey);
   });
 
-  test('Test bubbleSort on decending numerical order.', () => {
+  it('Test bubbleSort on decending numerical order.', () => {
     const input = [[6, 5, 6, 1, 2, 3, 0, 4]];
 
     const output = input.map((e) =>
@@ -66,10 +67,10 @@ describe('Test sort', () => {
 
     const answerKey = [[6, 6, 5, 4, 3, 2, 1, 0]];
 
-    expect(output).toStrictEqual(answerKey);
+    expect(output).to.deep.equal(answerKey);
   });
 
-  test('Test mergeSort on ascending lexicographical order.', () => {
+  it('Test mergeSort on ascending lexicographical order.', () => {
     const input = [
       ['Volvo', 'Ford', 'BMW', 'Mazda'],
       ['one', 'two', 'three', 'four', 'five', 'five'],
@@ -82,10 +83,10 @@ describe('Test sort', () => {
       ['five', 'five', 'four', 'one', 'three', 'two'],
     ];
 
-    expect(output).toStrictEqual(answerKey);
+    expect(output).to.deep.equal(answerKey);
   });
 
-  test('Test mergeSort on decending lexicographical order.', () => {
+  it('Test mergeSort on decending lexicographical order.', () => {
     const input = [
       ['Volvo', 'Ford', 'BMW', 'Mazda'],
       ['one', 'two', 'three', 'four', 'five', 'five'],
@@ -100,20 +101,20 @@ describe('Test sort', () => {
       ['two', 'three', 'one', 'four', 'five', 'five'],
     ];
 
-    expect(output).toStrictEqual(answerKey);
+    expect(output).to.deep.equal(answerKey);
   });
 
-  test('Test mergeSort on ascending numerical order.', () => {
+  it('Test mergeSort on ascending numerical order.', () => {
     const input = [[6, 5, 6, 1, 2, 3, 0, 4]];
 
     const output = input.map((e) => mergeSort(e, comparatorNumeric()));
 
     const answerKey = [[0, 1, 2, 3, 4, 5, 6, 6]];
 
-    expect(output).toStrictEqual(answerKey);
+    expect(output).to.deep.equal(answerKey);
   });
 
-  test('Test mergeSort on decending numerical order.', () => {
+  it('Test mergeSort on decending numerical order.', () => {
     const input = [[6, 5, 6, 1, 2, 3, 0, 4]];
 
     const output = input.map((e) =>
@@ -122,20 +123,20 @@ describe('Test sort', () => {
 
     const answerKey = [[6, 6, 5, 4, 3, 2, 1, 0]];
 
-    expect(output).toStrictEqual(answerKey);
+    expect(output).to.deep.equal(answerKey);
   });
 
-  test('Test partialSort on ascending numerical order.', () => {
+  it('Test partialSort on ascending numerical order.', () => {
     const input = [[6, 5, 6, 1, 2, 3, 0, 4]];
 
     const output = input.map((e) => partialSort(e, comparatorNumeric(), 2));
 
     const answerKey = [[0, 1, 6, 5, 6, 2, 3, 4]];
 
-    expect(output).toStrictEqual(answerKey);
+    expect(output).to.deep.equal(answerKey);
   });
 
-  test('Test partialSort on decending numerical order.', () => {
+  it('Test partialSort on decending numerical order.', () => {
     const input = [[3, 5, 6, 1, 2, 3, 0, 4]];
 
     const output = input.map((e) =>
@@ -144,6 +145,6 @@ describe('Test sort', () => {
 
     const answerKey = [[6, 5, 3, 4, 3, 1, 2, 0]];
 
-    expect(output).toStrictEqual(answerKey);
+    expect(output).to.deep.equal(answerKey);
   });
 });
